3df01c67b44a2c1796852fd4a751d794f26d7f1f
Joana Teixeira <joanajteixeira@outlook.com>
2020-11-01 14:38:33 +0000

    Updates

---
+++ precache-manifest.7e7766172e4189a5f6713ca79278ae6c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0f271ea1d0d7e6b412aa0fad909bdb3",
+    "url": "/nutritional-assessment/index.html"
+  },
+  {
+    "revision": "f614b077664f6086472c",
+    "url": "/nutritional-assessment/static/css/main.84f6798d.chunk.css"
+  },
+  {
+    "revision": "662e4291124e9ab76256",
+    "url": "/nutritional-assessment/static/js/2.82fe28cc.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/nutritional-assessment/static/js/2.82fe28cc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f614b077664f6086472c",
+    "url": "/nutritional-assessment/static/js/main.1f2da288.chunk.js"
+  },
+  {
+    "revision": "b7a56f33690b2673f605",
+    "url": "/nutritional-assessment/static/js/runtime-main.9bc45abb.js"
+  }
+]);
